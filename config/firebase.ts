8509d0cad9f5ea7d2ed33bbff706aec054a02857
Rean Jericho Guinto <reanjericho@gmail.com>
2026-04-17 19:28:52 +0800

    added a dotenv

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHVyYBupJ7G7-bneYuwXsibqfFA5IzLvA",
-  authDomain: "tulong-app-c7aaa.firebaseapp.com",
-  projectId: "tulong-app-c7aaa",
-  storageBucket: "tulong-app-c7aaa.firebasestorage.app",
-  messagingSenderId: "499151598398",
-  appId: "1:499151598398:web:49919f7287e5105272726f"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Prevents duplicate Firebase initialization in Next.js
